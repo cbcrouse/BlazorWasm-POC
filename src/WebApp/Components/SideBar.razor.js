6ef0555c14603fda6251942fadc2caf0e4d9e1a4
Casey Crouse <crouse.casey@gmail.com>
2022-06-14 22:12:55 -0700

    New Dashboard Layout with isolated JS

---
+++ src/WebApp/Components/SideBar.razor.js
@@ -0,0 +1,7 @@
+﻿export function toggleSidebar() {
+    let sidebar = document.querySelector(".sidebar");
+    let homeContent = document.querySelector(".home_content");
+
+    sidebar.classList.toggle("active");
+    homeContent.classList.toggle("active");
+}
